e7af372cb376402e026e1d26c208739f83db542b
Anjali Garg <anjali@headerlabs.com>
2016-08-29 12:27:52 +0530

    sqlite configuraion

---
+++ app/app.ts
@@ -0,0 +1,33 @@
+
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar, SQLite} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+ 
+@Component({
+    template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+    rootPage: any = HomePage;
+ 
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            StatusBar.styleDefault();
+            let db = new SQLite();
+            db.openDatabase({
+                name: "data.db",
+                location: "default"
+            }).then(() => {
+                db.executeSql("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)", {}).then((data) => {
+                    console.log("TABLE CREATED: ", data);
+                }, (error) => {
+                    console.error("Unable to execute sql", error);
+                })
+            }, (error) => {
+                console.error("Unable to open database", error);
+            });
+        });
+    }
+}
+ 
+ionicBootstrap(MyApp);
